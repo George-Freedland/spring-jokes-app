191d817c7f2333bbbb910cfa081940ed0e13354d
George-Freedland <george.freedland@gmail.com>
2021-05-13 18:15:23 -0700

    fixed joke gen dependeny injection in service

--- src/main/java/me/gfreedland/springjokesapp/services/JokeServiceImpl.java
+++ src/main/java/me/gfreedland/springjokesapp/services/JokeServiceImpl.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String sayJoke() {
-        ChuckNorrisQuotes cnq = new ChuckNorrisQuotes();
-        return cnq.getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
